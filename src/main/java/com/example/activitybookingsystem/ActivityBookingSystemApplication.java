8a37eef19f55cac10c1c058e4b1b0dabda4dc077
pg13824 <15571561+pg13824@user.noreply.gitee.com>
2026-05-21 10:17:38 +0800

    Initial commit

---
+++ src/main/java/com/example/activitybookingsystem/ActivityBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitybookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityBookingSystemApplication.class, args);
+	}
+
+}
